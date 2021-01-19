80d4999f3c92446cc29660b7a8392850bcde7601
lijiayan <1131106863@qq.com>
2021-01-19 22:19:26 +0800

    sketch0-LED5

---
+++ Sketchbook/five_led.ino/five_led.ino.ino
@@ -0,0 +1,50 @@
+/*
+ * Control 5 LED
+ */
+ int del = 100; //定义延时100ms
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(8, HIGH);
+  delay(del);
+  digitalWrite(8, LOW);
+  digitalWrite(9, HIGH);
+  delay(del);
+  digitalWrite(9, LOW);
+  digitalWrite(10, HIGH);
+  delay(del);
+  digitalWrite(10, LOW);
+  digitalWrite(11, HIGH);
+  delay(del);
+  digitalWrite(11, LOW);
+  digitalWrite(12, HIGH);
+  delay(del);
+  digitalWrite(12, LOW);
+  digitalWrite(11, HIGH);
+   delay(del);
+  digitalWrite(11, LOW);
+  digitalWrite(10, HIGH);
+  delay(del);
+  digitalWrite(10, LOW);
+  digitalWrite(9, HIGH);
+  delay(del);
+  digitalWrite(10, LOW);
+  digitalWrite(9, HIGH);
+  delay(del);
+  digitalWrite(9, LOW);
+  digitalWrite(8, HIGH);
+  delay(del);
+  digitalWrite(7, LOW);
+
+   
+  
+
+}
